0cd827fd6648f121cf07a81b3f2fc196dfbee953
unknown <collinsolads@gmail.com>
2025-01-12 16:03:32 +0100

    dashboard: progress 87%

--- src/components/Quickaction/QuickAction.jsx
+++ src/components/Quickaction/QuickAction.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/prop-types */
 import "../Quickaction/QuickAction.scss";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import {
   MdArrowDownward,
   MdArrowUpward,
@@ -10,9 +11,19 @@ import { FiArrowDownLeft } from "react-icons/fi";
 import { AiFillGift } from "react-icons/ai";
 import Carousel from "../Carousel/Carousel";
 import Cryptos from "../Cryptos/Cryptos";
+import { useState } from "react";
+import { LiaTimesSolid } from "react-icons/lia";
+import { Modal } from "@mui/material";
+import Naira from "../../assets/modals/naira.svg";
+import USD from "../../assets/modals/usd.svg";
+import Crypto from "../../assets/modals/cryptoC.svg";
 
 const QuickAction = () => {
-  // const location = useLocation();
+  const [depositmodal, setDepositModal] = useState(false);
+  const [opennairadep, setOpenNairaDep] = useState(false);
+
+  const [withdrawmodal, setWithdrawModal] = useState(false);
+  const [openwithdraw, setOpenWithdraw] = useState(false);
 
   return (
     <div className="quickaction">
@@ -40,21 +51,21 @@ const QuickAction = () => {
           <p>Swap Crypto</p>
         </Link>
 
-        <Link to="">
+        <Link to="" onClick={() => setDepositModal(true)}>
           <div className="icon-case">
             <MdArrowDownward className="icon" />
           </div>
           <p>Deposit</p>
         </Link>
 
-        <Link to="">
+        <Link to="/giftcards">
           <div className="icon-case">
             <AiFillGift className="icon" />
           </div>
           <p>Giftcard</p>
         </Link>
 
-        <Link to="">
+        <Link to="" onClick={() => setWithdrawModal(true)}>
           <div className="icon-case">
             <MdArrowUpward className="icon" />
           </div>
@@ -70,7 +81,168 @@ const QuickAction = () => {
         <Cryptos />
       </div>
       <Outlet />
+
+      <div className="modal">
+        <Modal
+          disableAutoFocus
+          open={depositmodal}
+          onClose={() => setDepositModal(false)}
+          sx={{ background: "rgba(114, 113, 113, 0.58)" }}
+        >
+          <div className="deposit-modal">
+            <div className="quit-btn" onClick={() => setDepositModal(false)}>
+              <LiaTimesSolid />
+            </div>
+
+            <h3>Deposit</h3>
+            <p>
+              At Jagaban we allow diversity, hence users have three options to
+              deposit funds into their Jagaban account
+            </p>
+
+            <div className="dp-method">
+              {/* Naira Deposit */}
+              <div className="box" onClick={() => setOpenNairaDep(true)}>
+                <div className="img">
+                  <img src={Naira} alt="Naira" />
+                </div>
+                <div className="currency">Naira</div>
+              </div>
+
+              {/* Other Deposit Methods */}
+              <div className="box">
+                <div className="img">
+                  <img src={USD} alt="USD" />
+                </div>
+                <div className="currency">USD</div>
+              </div>
+              <div className="box">
+                <div className="img">
+                  <img src={Crypto} alt="Crypto" />
+                </div>
+                <div className="currency">Crypto</div>
+              </div>
+            </div>
+
+            {/* Child Modal */}
+            <ChildModal
+              setOpenNairaDep={setOpenNairaDep}
+              opennairadep={opennairadep}
+            />
+          </div>
+        </Modal>
+      </div>
+
+      <div className="modal">
+        <Modal
+          disableAutoFocus
+          open={withdrawmodal}
+          onClose={() => setWithdrawModal(false)}
+          sx={{ background: "rgba(114, 113, 113, 0.58)" }}
+        >
+          <div className="deposit-modal">
+            <div className="quit-btn" onClick={() => setWithdrawModal(false)}>
+              <LiaTimesSolid />
+            </div>
+
+            <h3>Withdrawal</h3>
+            <p>
+              At Jagaban we allow diversity, hence users have two options to
+              withdraw funds into their Jagaban account
+            </p>
+
+            <div className="dp-method">
+              {/* Naira Deposit */}
+              <div className="box" onClick={() => setOpenWithdraw(true)}>
+                <div className="img">
+                  <img src={Naira} alt="Naira" />
+                </div>
+                <div className="currency">Naira</div>
+              </div>
+
+              <div className="box">
+                <div className="img">
+                  <img src={Crypto} alt="Crypto" />
+                </div>
+                <div className="currency">Crypto</div>
+              </div>
+            </div>
+
+            {/* Child Modal */}
+            <WithdrawModal
+              setOpenWithdraw={setOpenWithdraw}
+              openwithdraw={openwithdraw}
+            />
+          </div>
+        </Modal>
+      </div>
     </div>
+  );
+};
+
+const ChildModal = ({ setOpenNairaDep, opennairadep }) => {
+  const navigate = useNavigate();
+  return (
+    <Modal
+      disableAutoFocus
+      open={opennairadep}
+      onClose={() => setOpenNairaDep(false)}
+      sx={{ background: "rgba(0, 0, 0, 0.5)" }}
+    >
+      <div className="naira-deposit">
+        <div className="title">
+          <div className="head">Select Bank</div>
+
+          <div className="quit-btn" onClick={() => setOpenNairaDep(false)}>
+            <LiaTimesSolid className="icon" />
+          </div>
+        </div>
+        <p>
+          Kindly select the bank that you would made your deposit from to your
+          Jagaban account.
+        </p>
+
+        <div
+          className="add-acct"
+          onClick={() => navigate("/more/?option=preference")}
+        >
+          Add Account
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+const WithdrawModal = ({ setOpenWithdraw, openwithdraw }) => {
+  const navigate = useNavigate();
+  return (
+    <Modal
+      disableAutoFocus
+      open={openwithdraw}
+      onClose={() => setOpenWithdraw(false)}
+      sx={{ background: "rgba(0, 0, 0, 0.5)" }}
+    >
+      <div className="naira-deposit">
+        <div className="title">
+          <div className="head">Select Bank</div>
+
+          <div className="quit-btn" onClick={() => setOpenWithdraw(false)}>
+            <LiaTimesSolid className="icon" />
+          </div>
+        </div>
+        <p>
+          Kindly select the bank that you would made your deposit from to your
+          Jagaban account.
+        </p>
+
+        <div
+          className="add-acct"
+          onClick={() => navigate("/more/?option=preference")}
+        >
+          Add Account
+        </div>
+      </div>
+    </Modal>
   );
 };
 
--- src/pages/Sell/Sell.jsx
+++ src/pages/Sell/Sell.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import "../Sell/Sell.scss";
 import EmptyState from "../../assets/empty-state.svg";
 import BCH from "../../assets/coins/BCH.png";
@@ -8,10 +9,18 @@ import Positive from "../../assets/positive-graph.svg";
 import { NavLink, useSearchParams } from "react-router-dom";
 import { useState } from "react";
 import { FaAngleDown } from "react-icons/fa6";
+import { Modal } from "@mui/material";
+import Danger from "../../assets/modals/Danger.svg";
+import { LiaTimesSolid } from "react-icons/lia";
 
 const Sell = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [option, setOption] = useState(searchParams.get("option") || "sell");
+
+  const [sellmodal, setSellModal] = useState(false);
+  const [Currency, setCurrency] = useState("NGN");
+  const [opensetcurrency, setOpenSetCurrency] = useState(false);
+  const [selected, setSelected] = useState(false);
 
   const handleOptionChange = (newOption) => {
     setOption(newOption);
@@ -111,7 +120,7 @@ const Sell = () => {
               <div className="tokens-list">
                 {tokenInfo &&
                   tokenInfo.map((token, id) => (
-                    <li key={id}>
+                    <li key={id} onClick={() => setSelected(true)}>
                       <div className="tkn">
                         <div className="logo">
                           <img src={token.image} alt="" />
@@ -138,105 +147,188 @@ const Sell = () => {
         </div>
 
         <div className="action">
-          {/* <div className="sel-crypto-nil">
-            <img src={EmptyState} alt="" />
-          </div> */}
+          {selected ? (
+            <div className="sel-crypto-filled">
+              <div className="select-pane">
+                <NavLink
+                  to="/"
+                  className={option === "buy" ? "active-link" : ""}
+                  onClick={() => handleOptionChange("buy")}
+                >
+                  <p>Buy</p>
+                </NavLink>
 
-          <div className="sel-crypto-filled">
-            <div className="select-pane">
-              <NavLink
-                to="?option=buy"
-                className={option === "buy" ? "active-link" : ""}
-                onClick={() => handleOptionChange("buy")}
-              >
-                <p>Buy</p>
-              </NavLink>
+                <NavLink
+                  to="?option=sell"
+                  className={option === "sell" ? "active-link" : ""}
+                  onClick={() => handleOptionChange("sell")}
+                >
+                  <p>Sell</p>
+                </NavLink>
 
-              <NavLink
-                to="?option=sell"
-                className={option === "sell" ? "active-link" : ""}
-                onClick={() => handleOptionChange("sell")}
-              >
-                <p>Sell</p>
-              </NavLink>
-
-              <NavLink
-                to="?option=swap"
-                className={option === "swap" ? "active-link" : ""}
-                onClick={() => handleOptionChange("swap")}
-              >
-                <p>Swap</p>
-              </NavLink>
-            </div>
-
-            <div className="contents">
-              <div className="from">
-                <p>From</p>
-
-                <h3>BCH Wallet</h3>
+                <NavLink
+                  to="/"
+                  className={option === "swap" ? "active-link" : ""}
+                  onClick={() => handleOptionChange("swap")}
+                >
+                  <p>Swap</p>
+                </NavLink>
               </div>
 
-              <div className="to">
-                <p>To</p>
+              <div className="contents">
+                <div className="from">
+                  <p>From</p>
 
-                <h3>Naira Wallet</h3>
-              </div>
+                  <h3>BCH Wallet</h3>
+                </div>
 
-              <div className="rate">
-                <div className="hr-line"></div>
-                <p>Rate: 1605/$</p>
-              </div>
+                <div className="to">
+                  <p>To</p>
 
-              <div className="balance">Balance: 0 BCH</div>
+                  <h3>Naira Wallet</h3>
+                </div>
 
-              <div className="clear"></div>
+                <div className="rate">
+                  <div className="hr-line"></div>
+                  <p>Rate: 1605/$</p>
+                </div>
 
-              <div className="input-amt">
-                <input type="text" name="" />
+                <div className="balance">Balance: 0 BCH</div>
 
-                <div className="select-opt">
-                  <div className="cur-pair">
-                    <div className="cur">USD</div>
+                <div className="clear"></div>
+
+                <div className="input-amt">
+                  <input type="text" name="" />
+
+                  <div className="select-opt">
+                    <div
+                      className="cur-pair"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setOpenSetCurrency(!opensetcurrency);
+                      }}
+                    >
+                      <div className="cur">{Currency}</div>
+
+                      <FaAngleDown className="icon" />
+                    </div>
+                  </div>
+
+                  {opensetcurrency && (
+                    <div
+                      className="options"
+                      onClick={(e) => e.stopPropagation()}
+                    >
+                      <p className="head">Wallet</p>
+
+                      <div className="opts">
+                        <p
+                          onClick={() => {
+                            setCurrency("USD"), setOpenSetCurrency(false);
+                          }}
+                        >
+                          <input type="radio" checked={Currency === "USD"} />{" "}
+                          USD
+                        </p>
+
+                        <p
+                          onClick={() => {
+                            setCurrency("NGN"), setOpenSetCurrency(false);
+                          }}
+                        >
+                          <input type="radio" checked={Currency === "NGN"} />{" "}
+                          NGN
+                        </p>
+                      </div>
+                    </div>
+                  )}
+                </div>
+
+                <div className="perc-rate">
+                  <div className="card">25%</div>
+                  <div className="card">50%</div>
+                  <div className="card">75%</div>
+                  <div className="card">MAX</div>
+                </div>
+
+                <div className="coin-amt">
+                  <input type="text" name="" />
+
+                  <div className="select-opt">
+                    <div className="coin-pair">
+                      <div className="cur">BCH</div>
+                      <img src={BCH} alt="" />{" "}
+                    </div>
 
                     <FaAngleDown className="icon" />
                   </div>
-
-                  {/* <div className="options">
-                  <p>Wallet</p>
-
-                  <div className="opts">
-                    <p>USD</p>
-                    <p>NGN</p>
-                  </div>
-                </div> */}
                 </div>
-              </div>
 
-              <div className="perc-rate">
-                <div className="card">25%</div>
-                <div className="card">25%</div>
-                <div className="card">25%</div>
-                <div className="card">25%</div>
-              </div>
-
-              <div className="coin-amt">
-                <input type="text" name="" />
-
-                <div className="select-opt">
-                  <div className="coin-pair">
-                    <div className="cur">BCH</div>
-                    <img src={BCH} alt="" />{" "}
-                  </div>
-
-                  <FaAngleDown className="icon" />
+                <div className="cont" onClick={() => setSellModal(true)}>
+                  Continue
                 </div>
-              </div>
 
-              <div className="cont">Continue</div>
+                <ModalContents
+                  sellmodal={sellmodal}
+                  setSellModal={setSellModal}
+                />
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className="sel-crypto-nil">
+              <img src={EmptyState} alt="" />
+            </div>
+          )}
         </div>
       </div>
+    </div>
+  );
+};
+
+const ModalContents = ({ setSellModal, sellmodal }) => {
+  return (
+    <div className="">
+      <Modal
+        disableAutoFocus
+        open={sellmodal}
+        onClose={() => setSellModal(false)}
+        sx={{ background: "rgba(114, 113, 113, 0.58)" }}
+      >
+        <div className="sell-modal">
+          <div className="quit-btn" onClick={() => setSellModal(false)}>
+            <LiaTimesSolid />
+          </div>
+
+          <div className="clear"></div>
+
+          <div className="danger-img">
+            <img src={Danger} alt="" />
+          </div>
+
+          <div className="pop-info">
+            <h3>Confirm Transaction</h3>
+            <p>Confirm the sell of BCH</p>
+          </div>
+
+          <div className="snd">
+            <p>
+              You Pay: <br />
+              <span>BCH</span>
+            </p>
+          </div>
+
+          <div className="receive">
+            <p>
+              You Get <br />
+              <span>NGN 0</span>
+            </p>
+          </div>
+
+          <div className="continue" onClick={() => setSellModal(false)}>
+            Continue
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };

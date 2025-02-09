b4af81e309a4161d268f87afd47e94e1e10fa0bc
unknown <collinsolads@gmail.com>
2025-02-09 11:33:59 +0100

    dashboard responsiveness: progress 75%

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,29 @@
+import { IoMdNotifications } from "react-icons/io";
 import "../Navbar/Navbar.scss";
+import { LiaBarsSolid, LiaTimesSolid } from "react-icons/lia";
 
 const Navbar = () => {
-  return <div className="navbar">
-    
-  </div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="top">
+          <div className="logo">
+            <p>Jagaban</p>
+          </div>
+
+          <div className="btn-notif">
+            <div className="btn">
+              <LiaBarsSolid className="icon" />
+            </div>
+
+            <div className="notif">
+              <IoMdNotifications className="icon" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

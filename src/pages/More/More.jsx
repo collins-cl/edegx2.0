48650da23d445e8691eec8e6eb6aa44ee0f6133a
unknown <collinsolads@gmail.com>
2025-02-14 01:51:32 +0100

    dashboard responsiveness: progress 90%

--- src/pages/More/More.jsx
+++ src/pages/More/More.jsx
@@ -27,45 +27,47 @@ const More = () => {
     <div className="more">
       <div className="wrapper">
         <div className="title">
-          <NavLink
-            to="?option=profile"
-            className={option === "profile" ? "active-link" : ""}
-            onClick={() => handleOptionChange("profile")}
-          >
-            <p>Profile</p>
-          </NavLink>
+          <div className="container">
+            <NavLink
+              to="?option=profile"
+              className={option === "profile" ? "active-link" : ""}
+              onClick={() => handleOptionChange("profile")}
+            >
+              <p>Profile</p>
+            </NavLink>
 
-          <NavLink
-            to="?option=security"
-            className={option === "security" ? "active-link" : ""}
-            onClick={() => handleOptionChange("security")}
-          >
-            <p>Security</p>
-          </NavLink>
+            <NavLink
+              to="?option=security"
+              className={option === "security" ? "active-link" : ""}
+              onClick={() => handleOptionChange("security")}
+            >
+              <p>Security</p>
+            </NavLink>
 
-          <NavLink
-            to="?option=preference"
-            className={option === "preference" ? "active-link" : ""}
-            onClick={() => handleOptionChange("preference")}
-          >
-            <p>Preference</p>
-          </NavLink>
+            <NavLink
+              to="?option=preference"
+              className={option === "preference" ? "active-link" : ""}
+              onClick={() => handleOptionChange("preference")}
+            >
+              <p>Preference</p>
+            </NavLink>
 
-          <NavLink
-            to="?option=refer"
-            className={option === "refer" ? "active-link" : ""}
-            onClick={() => handleOptionChange("refer")}
-          >
-            <p>Refer</p>
-          </NavLink>
+            <NavLink
+              to="?option=refer"
+              className={option === "refer" ? "active-link" : ""}
+              onClick={() => handleOptionChange("refer")}
+            >
+              <p>Refer</p>
+            </NavLink>
 
-          <NavLink
-            to="?option=others"
-            className={option === "others" ? "active-link" : ""}
-            onClick={() => handleOptionChange("others")}
-          >
-            <p>Others</p>
-          </NavLink>
+            <NavLink
+              to="?option=others"
+              className={option === "others" ? "active-link" : ""}
+              onClick={() => handleOptionChange("others")}
+            >
+              <p>Others</p>
+            </NavLink>
+          </div>
         </div>
 
         <div className="contents">
